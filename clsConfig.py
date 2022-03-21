e8411be2c00161384d89a378258a7967b66022eb
SatyakiDe2019 <49108072+SatyakiDe2019@users.noreply.github.com>
2022-03-21 17:03:21 -0400

    Configuration file.

---
+++ clsConfig.py
@@ -0,0 +1,40 @@
+################################################
+#### Written By: SATYAKI DE                 ####
+#### Written On:  15-May-2020               ####
+#### Modified On: 28-Dec-2021               ####
+####                                        ####
+#### Objective: This script is a config     ####
+#### file, contains all the keys for        ####
+#### Machine-Learning & streaming dashboard.####
+####                                        ####
+################################################
+
+import os
+import platform as pl
+
+class clsConfig(object):
+    Curr_Path = os.path.dirname(os.path.realpath(__file__))
+
+    os_det = pl.system()
+    if os_det == "Windows":
+        sep = '\\'
+    else:
+        sep = '/'
+
+    conf = {
+        'APP_ID': 1,
+        'ARCH_DIR': Curr_Path + sep + 'arch' + sep,
+        'PROFILE_PATH': Curr_Path + sep + 'profile' + sep,
+        'LOG_PATH': Curr_Path + sep + 'log' + sep,
+        'REPORT_PATH': Curr_Path + sep + 'report',
+        'FILE_NAME': Curr_Path + sep + 'Image' + sep + 'Orig.jpeg',
+        'SRC_PATH': Curr_Path + sep + 'data' + sep,
+        'APP_DESC_1': 'Old Video Enhancement!',
+        'DEBUG_IND': 'N',
+        'INIT_PATH': Curr_Path,
+        'SUBDIR': 'data',
+        'SEP': sep,
+        'COIN_DEF_HEIGHT':0.22,
+        'PIC_TO_CM_MAP': 15.24,
+        'CONTOUR_AREA': 2000
+    }
